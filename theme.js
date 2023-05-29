0a2ebaa008f870384cf78c71f751317ece3e7a25
VihanVla <111562310+VihanVla@users.noreply.github.com>
2023-05-29 10:46:32 +0330

    local storage

--- theme.js
+++ theme.js
@@ -1,5 +1,8 @@
 let themes = document.querySelector(".themes")
 let selection = document.querySelector("#selection")
+let selectedTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"
+selection.textContent = selectedTheme
+document.body.className = selectedTheme
 
 Array.from(themes.children).forEach(them => {
 
@@ -8,6 +11,7 @@ Array.from(themes.children).forEach(them => {
         let color = e.target.dataset.color
         document.body.className = color
         selection.textContent = color
+        localStorage.setItem("theme", color)
 
     })
 })
